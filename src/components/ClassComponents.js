633e38da3b19704f8404f7eee95aea5446aaa57b
ZaidohDev <danfealmeida@gmail.com>
2021-04-16 11:38:53 -0300

    Initialize project using Create React App

---
+++ src/components/ClassComponents.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+export default class ClassComponent extends Component {
+
+  remder(){
+    return (
+      <div>
+        <p>ClassComponent</p>
+      </div>
+    )
+  }
+}
